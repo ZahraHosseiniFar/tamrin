d41d1c21ff87c372ed2aedfb3b11b52ba7b0b3be
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 18:10:40 +0330

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter one number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isTrain(a));
+            Console.ReadKey();
+        }
+
+        private static Boolean isTrain(int a)
+        {
+            int i;
+            int sum = 0;
+            for (i = 0; i <= a; i++)
+            {
+                sum = sum + i;
+
+                if (sum == a)
+                {
+                    return true;
+                }
+            }
+                    return false;
+            
+        }
+    }
+}
