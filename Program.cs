a8d444f78ee26b5c0fbcfed014b8db7f8450227e
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 18:05:09 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter one number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isSquare(a));
+            Console.ReadKey();
+        }
+
+        private static Boolean isSquare(int x)
+        {
+            int i;
+            for (i = 1; i <= (x / 2); i++)
+            {
+                if (x == i * i)
+                    return true;
+            }
+            return false;
+        }
+    }
+   
+}
